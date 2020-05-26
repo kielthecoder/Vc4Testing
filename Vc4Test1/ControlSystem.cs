fe8d098ee1907c26c965e1b9e2a0e15968ba89b8
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2020-05-26 15:13:36 -0400

    XPanel with power button

--- Vc4Test1/ControlSystem.cs
+++ Vc4Test1/ControlSystem.cs
@@ -1,20 +1,27 @@
 using System;
-using Crestron.SimplSharp;                          	// For Basic SIMPL# Classes
-using Crestron.SimplSharpPro;                       	// For Basic SIMPL#Pro classes
-using Crestron.SimplSharpPro.CrestronThread;        	// For Threading
-using Crestron.SimplSharpPro.Diagnostics;		    	// For System Monitor Access
-using Crestron.SimplSharpPro.DeviceSupport;         	// For Generic Device Support
+using Crestron.SimplSharp;
+using Crestron.SimplSharpPro;
+using Crestron.SimplSharpPro.CrestronThread;
+using Crestron.SimplSharpPro.Diagnostics;
+using Crestron.SimplSharpPro.DeviceSupport;
+using Crestron.SimplSharpPro.UI;
 
 namespace Vc4Test1
 {
     public class ControlSystem : CrestronControlSystem
     {
+        private XpanelForSmartGraphics tp1;
+        private bool bSystemPowerOn;
+
         public ControlSystem()
             : base()
         {
             try
             {
                 Thread.MaxNumberOfUserThreads = 20;
+
+                // System defaults to OFF
+                bSystemPowerOn = false;
             }
             catch (Exception e)
             {
@@ -26,11 +33,34 @@ namespace Vc4Test1
         {
             try
             {
+                tp1 = new XpanelForSmartGraphics(0x03, this);
+                tp1.SigChange += tp_SigChange;
+                tp1.BooleanOutput[12].UserObject = new Action<bool>(b => { if (b) ToggleSystemPower(); });
+                tp1.Register();
             }
             catch (Exception e)
             {
                 ErrorLog.Error("Error in InitializeSystem: {0}", e.Message);
             }
+        }
+
+        public void tp_SigChange(BasicTriList dev, SigEventArgs args)
+        {
+            var obj = args.Sig.UserObject;
+
+            if (obj is Action<bool>)
+            {
+                var func = (Action<bool>)obj;
+                func(args.Sig.BoolValue);
+            }
+        }
+
+        void ToggleSystemPower()
+        {
+            bSystemPowerOn = !bSystemPowerOn;
+
+            tp1.BooleanInput[10].BoolValue = bSystemPowerOn;
+            tp1.BooleanInput[11].BoolValue = !bSystemPowerOn;
         }
     }
 }
