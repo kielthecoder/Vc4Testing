b7eecbf8d8e77bcee7b779e4bd3a426864327115
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2020-05-26 09:37:05 -0400

    bare minimum C# program

---
+++ Vc4Test1/ControlSystem.cs
@@ -0,0 +1,36 @@
+using System;
+using Crestron.SimplSharp;                          	// For Basic SIMPL# Classes
+using Crestron.SimplSharpPro;                       	// For Basic SIMPL#Pro classes
+using Crestron.SimplSharpPro.CrestronThread;        	// For Threading
+using Crestron.SimplSharpPro.Diagnostics;		    	// For System Monitor Access
+using Crestron.SimplSharpPro.DeviceSupport;         	// For Generic Device Support
+
+namespace Vc4Test1
+{
+    public class ControlSystem : CrestronControlSystem
+    {
+        public ControlSystem()
+            : base()
+        {
+            try
+            {
+                Thread.MaxNumberOfUserThreads = 20;
+            }
+            catch (Exception e)
+            {
+                ErrorLog.Error("Error in ControlSystem constructor: {0}", e.Message);
+            }
+        }
+
+        public override void InitializeSystem()
+        {
+            try
+            {
+            }
+            catch (Exception e)
+            {
+                ErrorLog.Error("Error in InitializeSystem: {0}", e.Message);
+            }
+        }
+    }
+}
